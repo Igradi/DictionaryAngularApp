4baa8caf53601ffbe6c7b44492bae2e3051cfeb4
akrapovicivan <iakrap00@fesb.hr>
2022-05-27 14:03:30 +0200

    First commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  invalidLogin: boolean | undefined;
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient) { }
+
+  login(form: NgForm) {
+    const credentials = {
+      'username': form.value.username,
+      'password': form.value.password
+    }
+    this.http.post("https://localhost:7153/api/auth/login", credentials)
+      .subscribe(response => {
+        const token = (<any>response).token;
+        localStorage.setItem("jwt", token);
+        this.invalidLogin = false;
+        this.router.navigate(["/"]);
+      }, err => {
+        this.invalidLogin = true;
+      })
+  }
 
   ngOnInit(): void {
   }
