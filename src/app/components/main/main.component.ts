54288280eafd8b309d9c7314c980816393c1dd10
akrapovicivan <iakrap00@fesb.hr>
2022-06-06 09:33:48 +0200

    updated margin on main and icons

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,8 +1,12 @@
+
 import { Component, OnInit } from '@angular/core';
 import { RandomWordService } from "../../shared/random-word.service";
 import { Word } from "../../shared/word.model";
 import { WordDefinitionService } from "../../shared/word-definition.service";
 import { NULL_EXPR } from '@angular/compiler/src/output/output_ast';
+import { ConnectableObservable } from 'rxjs';
+import { ThisReceiver } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-main',
@@ -21,29 +25,27 @@ export class MainComponent implements OnInit {
   ngOnInit(): void {
     this._randomWord.getWord().subscribe(
       data => {
-        console.log(data);
         this.word.wordString = data.word;
         this.word.definition = data.results[0].definition;
-        this.word.synonyms = data.results[0].synonyms;
         this.word.wordType = data.results[0].partOfSpeech;
-        for (let i = 0; i < data.results.length; i++) {
-          if (data.results[i].example == undefined) {
-            continue;
-          } else {
-            this.word.example = data.results[i].example;
-            break;
+        try {
+          this.word.example = data.results[0].examples[0];
+        }
+        catch (err) {
+          this.word.example = "  Sorry, we havent found any example for this word";
+        } finally {
+          try {
+            this.word.synonyms = data.results[0].synonyms;
+          }
+          catch (err) {
+            this.word.synonyms[0] = " This word has no synonyms";
           }
         }
-        console.log(this.word.synonyms);
-        console.log(this.word.wordString);
-        this._wordDefinition.getWordExample(this.word.wordString).subscribe(
-          dta => {
-
-          }
-        )
+        if (this.word.synonyms == undefined) {
+          this.word.synonyms = [" This word has no synonyms"];
+        }
       }
     )
+
   }
 }
-
-
