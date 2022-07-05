79130714723eaa53438cb3631cb30183b2cc4e2f
Ivor <ivorgradinski@gmail.com>
2022-07-05 18:25:27 +0200

    reset password

--- src/app/components/reset-password-form/reset-password-form.component.ts
+++ src/app/components/reset-password-form/reset-password-form.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
-
+  onSubmitResetPassword(userInput: { token: string, password: string, confirmPassword: string }) {
+    this.http.post('https://localhost:7153/api/User/reset-password', userInput)
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
 }
